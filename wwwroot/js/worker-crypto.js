f5f5b768766de85957c93c48b153ce98d07d7bb7
uprun <geraclion@gmail.com>
2018-10-07 22:33:33 +0300

    Added web-worker that calculates RSA key.

--- wwwroot/js/worker-crypto.js
+++ wwwroot/js/worker-crypto.js
@@ -1,3 +1,11 @@
+importScripts(
+    'cryptico/jsbn.js', 
+    'cryptico/random.js', 
+    'cryptico/hash.js', 
+    'cryptico/rsa.js', 
+    'cryptico/aes.js', 
+    'cryptico/api.js'); 
+
 var GeneratePassPhrase = function(initialPhrase) {
     var prefix = "" + initialPhrase;
     for(var k = 0; k < 20; k++) {
@@ -6,4 +14,28 @@ var GeneratePassPhrase = function(initialPhrase) {
     return prefix;
 };
 
-cryptico.generateRSAKey(GeneratePassPhrase(), 2048);
+var rsaKey = cryptico.generateRSAKey(GeneratePassPhrase(), 2048);
+
+var restoreFieldsOfObject = function(objectInstance, savedFields) {
+    for( var a in objectInstance) {
+        if(typeof(savedFields[a]) != "undefined") {
+            if(typeof(objectInstance[a]) == "object"  ) {
+                objectInstance[a] = self.restoreFieldsOfObject(objectInstance[a], savedFields[a]);
+            }
+            else {
+                if(typeof(objectInstance[a]) != "function" ) {
+                    objectInstance[a] = savedFields[a];
+                }
+            }
+        }        
+    }
+    return objectInstance;
+};
+
+onmessage = function(e) {
+    rsaKey = restoreFieldsOfObject(rsaKey, e.data);
+    postMessage(rsaKey);
+};
+
+
+
