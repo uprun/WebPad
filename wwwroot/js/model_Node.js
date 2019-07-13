19cc3a4436308f9c5955339e5266b35821d11bd9
uprun <geraclion@gmail.com>
2019-07-13 18:05:58 +0300

    Working on showing connections.

--- wwwroot/js/model_Node.js
+++ wwwroot/js/model_Node.js
@@ -3,8 +3,6 @@ function model_Node(data)
     var self = this;
     self.id = data.id;
     self.text = ko.observable(data.text);
-    self.x = data.x;
-    self.y = data.y;
     self.color = data.color;
     self.background = data.background;
     if(typeof(self.background) == "undefined" || self.background == null)
@@ -19,8 +17,6 @@ function model_Node(data)
         return {
             id: self.id,
             text: self.text(),
-            x: self.x,
-            y: self.y,
             color: self.color,
             background: self.background
         };
