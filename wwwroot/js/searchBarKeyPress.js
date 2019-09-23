d3e23738ed7c1656c7269265c9209d0feff07653
Oleksandr Kryvonos <geraclion@gmail.com>
2019-09-23 21:26:12 +0300

    Added enter handler for search bar.

---
+++ wwwroot/js/searchBarKeyPress.js
@@ -0,0 +1,19 @@
+lookup.searchBarKeyPress = function(data, event) 
+{
+    if(event.shiftKey)
+    {
+        if(event.keyCode == 13)
+        {
+            lookup.addToStackOfSearch();
+        }
+
+    }
+    else
+    {
+        if(event.keyCode == 13)
+        {
+            lookup.CreateNoteFromSearchQuery();
+        }
+    }
+    return true;
+};
