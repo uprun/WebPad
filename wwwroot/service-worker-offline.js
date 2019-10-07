88117e817a826302bcdab4030e243323bf483dc5
uprun <geraclion@gmail.com>
2019-10-08 00:07:54 +0300

    latest version

--- wwwroot/service-worker-offline.js
+++ wwwroot/service-worker-offline.js
@@ -1,4 +1,4 @@
-var current_cache_name = "v2";
+var current_cache_name = "v3";
 self.addEventListener('install', event => {
     console.log(current_cache_name + ' installing…');
     event.waitUntil(
