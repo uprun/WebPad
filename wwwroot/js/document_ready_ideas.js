d8ee5c4c0c37261e6c7873c2c24f3349e1adc984
uprun <geraclion@gmail.com>
2020-02-04 00:37:55 +0200

    Changing layout in order tofully utilize screen space.

--- wwwroot/js/document_ready_ideas.js
+++ wwwroot/js/document_ready_ideas.js
@@ -3,5 +3,6 @@ $(document).ready(function()
     var viewModel = new ConnectedNotesViewModel();
     viewModel.ApplyLookupToSelf();
     ko.applyBindings(viewModel);
+    viewModel.recalculateColumnsCount();
     //setTimeout(viewModel.ActualGenerateConnections, 3000);
 });
---
+++ wwwroot/js/recalculateColumnsCount.js
@@ -0,0 +1,12 @@
+lookup.columnsCount = ko.observable(undefined);
+lookup.recalculateColumnsCount = function() {
+
+    var header_width = document.getElementById("webpad-header-absolute-position").offsetWidth;
+    var found_columns = document.getElementsByClassName("webpad-text-column");
+    if(found_columns.length > 0)
+    {
+        var column_width = found_columns[0].offsetWidth;
+        var calculatedColumns = header_width / column_width;
+        lookup.columnsCount(calculatedColumns);
+    }
+};
