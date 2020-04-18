74f079b0e88e6ffbc455f30e6eb789fdf7a44607
uprun <geraclion@gmail.com>
2020-04-18 11:56:24 +0300

    Added search button for quotes

--- wwwroot/js/mainPage.js
+++ wwwroot/js/mainPage.js
@@ -391,19 +391,7 @@ function ConnectedNotesViewModel()
         return ( lookup.localPrefix + (lookup.freeLocalIndex++) );
     };
 
-    lookup.SearchNotesQuery = ko.observable("");
-    lookup.SearchNotesQuery
-        .extend({ rateLimit: 200 });
-    lookup.SearchNotesQuery.subscribe(function(value){
-        console.log('clear');
-        // do not remove stack if card is just created
-        if(lookup.stackOfCards().length > 1 || value.trim().length > 0)
-        {
-            lookup.clearStackOfCards();
-            
-        }
-        
-    });
+    
     
     lookup.sortedByDateCards = ko.pureComputed(function()
     {
