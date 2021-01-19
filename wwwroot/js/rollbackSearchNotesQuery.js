d57b989542d9b1889a59a63eac13ac7b8025cf46
uprun <geraclion@gmail.com>
2021-01-19 23:36:11 +0200

    fixed rollback function

--- wwwroot/js/rollbackSearchNotesQuery.js
+++ wwwroot/js/rollbackSearchNotesQuery.js
@@ -1,12 +1,16 @@
 lookup.stackSearchNotesQuery = ko.observableArray([]);
 lookup.rollbackSearchNotesQuery = function()
 {
-    var previousQuery = "";
     if(lookup.stackSearchNotesQuery().length > 0)
     {
         previousQuery = lookup.stackSearchNotesQuery.pop();
+        lookup.onListChanged_setScrollTopOffset(previousQuery.scrollPosition);
+        lookup.SearchNotesQuery(previousQuery.query);
+        lookup.CurrentResultLimit(previousQuery.amountOfCardsLimit)
     }
-    lookup.onListChanged_setScrollTopOffset(previousQuery.scrollPosition);
-    lookup.SearchNotesQuery(previousQuery.query);
-    lookup.CurrentResultLimit(previousQuery.amountOfCardsLimit)
+    else
+    {
+        lookup.SearchNotesQuery("");
+    }
+    
 };
