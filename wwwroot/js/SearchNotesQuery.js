0b1b103e04c9c87c7d472042f647d83995ac5481
uprun <geraclion@gmail.com>
2021-01-24 13:00:23 +0200

    removed scroll to bottom on each change of query

--- wwwroot/js/SearchNotesQuery.js
+++ wwwroot/js/SearchNotesQuery.js
@@ -1,8 +1,3 @@
 lookup.SearchNotesQuery = ko.observable("");
 lookup.SearchNotesQuery
     .extend({ rateLimit: 150 });
-lookup.SearchNotesQuery
-    .subscribe(function(changes)
-    {
-        lookup.onListChanged_set_scrollToLatestCard();
-    });
