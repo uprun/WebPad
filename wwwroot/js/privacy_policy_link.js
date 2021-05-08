511ab169d951898bac6b696cd347cb1afa71b4ff
Oleksandr Kryvonos <geraclion@gmail.com>
2021-05-09 02:08:22 +0300

    fix for Mac privacy policy

--- wwwroot/js/privacy_policy_link.js
+++ wwwroot/js/privacy_policy_link.js
@@ -1,10 +1,10 @@
-lookup.platform_is_mobile = ko.observable(false);
+lookup.platform_is_cordova = ko.observable(false);
 
 lookup.check_platform = function()
 {
     if(typeof(window.cordova) !== 'undefined')
     {
-        lookup.platform_is_mobile(true);
+        lookup.platform_is_cordova(true);
     }
     // if (window.cordova && cordova.platformId !== "browser") {
     //     lookup.platform_is_mobile(true);
@@ -15,9 +15,21 @@ lookup.check_platform = function()
 
 lookup.privacy_policy_link = ko.computed(function()
 {
-    if(lookup.platform_is_mobile())
+    if(lookup.platform_is_cordova())
     {
-        return "file:///android_asset/www/privacy_policy.html";
+        //&& window.cordova.platformId !== "electron"
+        if(
+            typeof(navigator) !== 'undefined' && 
+            (navigator.platform === "MacIntel")
+        )
+        {
+            return "file:///Applications/WebPad.app/Contents/Resources/app.asar/privacy_policy_mac.html";
+        }
+        else
+        {
+            return "file:///android_asset/www/privacy_policy.html";
+        }
+        
     }
     else
     {
