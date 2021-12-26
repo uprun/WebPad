a6d867335cfde828dce60b68e12f6f9241de652e
Oleksandr Kryvonos <geraclion@gmail.com>
2021-12-26 19:10:21 +0200

    updating deploy command

--- command-deploy.sh
+++ command-deploy.sh
@@ -1,3 +1,3 @@
 systemctl stop  kestrel-asp.service &&
-dotnet publish -c Development --self-contained --runtime linux-x64 --framework netcoreapp3.1 -o bin-linux-2.2 &&
+dotnet publish -c Development --self-contained --runtime linux-x64 --framework netcoreapp6.0 -o bin-linux-2.2 &&
 systemctl start  kestrel-asp.service
