37920faf6ca8e31397aba31794b29b780abf70ba
Oleksandr Kryvonos <geraclion@gmail.com>
2021-12-26 18:52:49 +0200

    adding highlight opacity

--- wwwroot/js/model_Operation.js
+++ wwwroot/js/model_Operation.js
@@ -46,10 +46,43 @@ lookup.model_Operation = function(data)
     };
 };
 
+lookup.split_color_from_6_hex = function(color_in_6_hex)
+{
+    var hex_red = '0x' + color_in_6_hex.substring(1, 3);
+    var hex_green = '0x' + color_in_6_hex.substring(3, 5);
+    var hex_blue = '0x' + color_in_6_hex.substring(5, 7);
+    var int_red = parseInt(hex_red, 16);
+    var int_green = parseInt(hex_green, 16);
+    var int_blue = parseInt(hex_blue, 16);
+    var colorWithIntComponents = 
+    {
+        red: int_red,
+        green: int_green,
+        blue: int_blue
+    };
+    return colorWithIntComponents;
+};
+
+lookup.form_rgba_string_constant = function(color_with_components, alpha)
+{
+    var toReturn = 'rgba(' + color_with_components.red + ', ' + color_with_components.green + ', ' + color_with_components.blue + ', ' + alpha +')' ;
+    return toReturn;
+};
+
 lookup.prefill_Operation = function(self, abc) {
 
     self.color = abc.color;
     self.text = abc.text;
+    if(typeof(abc.color) === 'undefined')
+    {
+        self.color_border = "#ff12ff";
+    }
+    else
+    {
+        var color_with_components = lookup.split_color_from_6_hex(abc.color);
+
+        self.color_border = lookup.form_rgba_string_constant(color_with_components, '0.6')
+    }
     var test = abc.text.split(" ");
     self.textSplitted = ko.utils.arrayMap(test, function (item) {
         var toSearch = item
