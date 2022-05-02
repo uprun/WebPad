a445c65305e92eb6f6073052a87fa45d33847415
Oleksandr Kryvonos <geraclion@gmail.com>
2022-05-02 19:55:51 +0300

    no-parse of Alias

--- wwwroot/js/Operation_was_added.js
+++ wwwroot/js/Operation_was_added.js
@@ -3,5 +3,4 @@ lookup.Operation_was_added = function(data) {
     // backend-worker context
     var toAdd = new lookup.model_Operation(data);
     lookup.Operations.push(toAdd);
-    lookup.reply_from_backend_worker('saveAliasesToStorage.event', lookup.Aliases);
 };
