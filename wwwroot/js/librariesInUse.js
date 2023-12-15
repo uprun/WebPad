51b8c84f1512edf5e199b5c2e524fc77c36df187
Oleksandr Kryvonos <geraclion@gmail.com>
2023-12-15 23:14:56 +0100

    used tools section

---
+++ wwwroot/js/librariesInUse.js
@@ -0,0 +1,25 @@
+// as of date 2023-12-15
+lookup.librariesInUse = ko.observableArray(
+    [
+        {
+            "library": "TinySegmenter [Japanese tokeniser]",
+            "url": "http://chasen.org/~taku/software/TinySegmenter/"
+        },
+        {
+            "library": "Knockout.js [MVVM library, two-way bindings]",
+            "url": "https://knockoutjs.com/"
+        },
+        {
+            "library": "JQuery [simplified requests to DOM]",
+            "url" : "https://jquery.com/"
+        },
+        {
+            "library": "W3.CSS [CSS presets]",
+            "url": "https://www.w3schools.com/w3css/default.asp"
+        },
+        {
+            "library": "Apache Cordova [deploy web-site as an app]",
+            "url": "https://cordova.apache.org/"
+        }
+    ]
+);
