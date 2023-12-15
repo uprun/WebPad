438265cb4ca98fdb4416468b6f412b02a8313648
Oleksandr Kryvonos <geraclion@gmail.com>
2023-12-15 23:44:35 +0100

    trying debug config

--- command-deploy.sh
+++ command-deploy.sh
@@ -1,3 +1,3 @@
 systemctl stop  webpad.service &&
-dotnet publish -c Development --self-contained --runtime linux-x64 --framework netcoreapp6.0 -o bin-linux-2.2 &&
+dotnet publish -c Debug --self-contained --runtime linux-x64 --framework netcoreapp6.0 -o bin-linux-2.2 &&
 systemctl start  webpad.service
