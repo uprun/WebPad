14391e3089880ddb834c50048265336d2a0a4272
Oleksandr Kryvonos <geraclion@gmail.com>
2024-09-11 23:44:50 +0200

    now removing stuff

--- wwwroot/js/add_Alias.js
+++
@@ -1,9 +0,0 @@
-lookup.add_Alias = function(left, right)
-{
-    // backend-worker context
-    if( typeof(lookup.Aliases[left]) === 'undefined')
-    {
-        lookup.Aliases[left] = {};
-    }
-    lookup.Aliases[left][right] = true;
-};
