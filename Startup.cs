11be21c82de7a29e0d5e74f0b24c010fc6f7e37f
Oleksandr Kryvonos <geraclion@gmail.com>
2024-11-30 23:14:12 +0100

    2024-11-30--23h14m12s

--- Startup.cs
+++ Startup.cs
@@ -66,7 +66,7 @@ namespace WebPad
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=ideas}/{id?}",
+                    pattern: "{controller=Home}/{action=ideas}/{id?}"
                     );
             });
         }
