a65e79ed9a3bb600d5dcb04cd2f65b74aa72c4db
Oleksandr Kryvonos <geraclion@gmail.com>
2024-12-30 01:29:37 +0200

    cleanup (#21)

--- String_IsAbsent_Extension.cs
+++
@@ -1,9 +0,0 @@
-using System;
-namespace WebPad;
-public static class String_IsAbsent_Extension
-{
-    public static bool IsAbsent(this String value, string to_check)
-    {
-        return value.Contains(to_check) == false;
-    }
-}
