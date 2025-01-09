0dd598808221e4c1ad823366b4b868a2b6763a8a
Oleksandr Kryvonos <geraclion@gmail.com>
2025-01-09 23:03:40 +0100

    updated cordova script

--- cordova-prepare.sh
+++ cordova-prepare.sh
@@ -1,2 +1,2 @@
 cp -R -v wwwroot/* ../webpad-cordova/webpad/www/
-cp -v wwwroot/bundle_ideas.html ../webpad-cordova/webpad/www/index.html
+cp -v wwwroot/WebPad-mini-wiki.html ../webpad-cordova/webpad/www/index.html
