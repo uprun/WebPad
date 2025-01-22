c2158accbc4a10d7a99de60b42b7daa8288d17a8
Oleksandr Kryvonos <geraclion@gmail.com>
2025-01-22 23:46:00 +0100

    no need for colour conversion

--- wwwroot/js/models/prefill_Operation.js
+++ wwwroot/js/models/prefill_Operation.js
@@ -1,17 +1,6 @@
-lookup.prefill_Operation = function(self, abc) {
-
+lookup.prefill_Operation = function(self, abc) 
+{
     self.color = abc.color;
     self.text = abc.text;
-    if(typeof(abc.color) === 'undefined')
-    {
-        self.color_border = "#ff12ff";
-    }
-    else
-    {
-        var color_with_components = lookup.split_color_from_6_hex(abc.color);
-
-        self.color_border = lookup.form_rgba_string_constant(color_with_components, '0.6')
-    }
-
     self.all_symbols = abc.text.split("");
 };
